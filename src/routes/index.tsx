a24709044f05b7a57d790c9674ed39db4c2aaf41
Weydla Alves <weydlaalves23@gmail.com>
2022-01-26 19:01:59 -0300

    SPLASH

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -22,9 +22,7 @@ export function AppRoutes() {
       <Screen name="home_working" component={HomeWorking} /> 
       <Screen name="home" component={Home} />  
       <Screen name="inicial" component={Inicial} />
-      <Screen name="home_working" component={HomeWorking} /> 
-      <Screen name="home" component={Home} />
-      
+     
       
       </Navigator>
     </NavigationContainer>
